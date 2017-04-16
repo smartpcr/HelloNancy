cfe3ac7bf6403921b26966556b24b6f3110cbde1
xiaodong li <xiaodongli@xd005.lan>
2017-04-16 13:16:05 -0700

    initial

---
+++ HelloWorld/CurrentTimeModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Nancy;
+
+namespace HelloWorld
+{
+    public class CurrentTimeModule : NancyModule
+    {
+        public CurrentTimeModule()
+        {
+            Get("/", _ => DateTime.UtcNow);
+        }
+    }
+}
---
+++ HelloWorld/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nancy.Owin;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseOwin(buildFunc =>
+            {
+                buildFunc(next => environment =>
+                {
+                    System.Console.WriteLine("Got request {0:yyyy-MM-dd HH:mm:ss}", DateTime.Now);
+                    return next(environment);
+                });
+
+                buildFunc.UseNancy(); 
+            });
+
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
